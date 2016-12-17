7129095ac7a9387c8d47c3244d8150260f1c3697
Modestas <mode.saulys@gmail.com>
2016-12-17 20:02:56 +0200

    services fix

--- server/services/get-all.sql
+++ server/services/get-all.sql
@@ -5,7 +5,6 @@ select
     s.working_hours_end as workStopTime,
     s.email as email,
     s.phone as phone,
-    s.description as description,
     ct.name as city
 from `service` s
     join city ct on s.city_id = ct.id

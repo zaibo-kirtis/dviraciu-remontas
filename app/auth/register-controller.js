83b45f7a6d67a0f3fa6e5c76497a8f95990d304c
Gvidas Janušonis <gvidasja@gmail.com>
2016-12-18 21:38:20 +0200

    update registration

--- app/auth/register-controller.js
+++ app/auth/register-controller.js
@@ -6,6 +6,14 @@ export function RegisterController( AuthService, $location, $controller) {
     this.model = {};
 
     this.register = function register() {
-        AuthService.register(self.model).then( () => $location.path( '/login' ));
+        AuthService.register(self.model).then(() => {
+            $location.path('/')
+        }, self.handleError);
     };
+
+    this.sexes = [
+        { id: 1, name: 'Vyras' },
+        { id: 2, name: 'Moteris' },
+        { id: 3, name: 'Kita' }
+    ]
 }

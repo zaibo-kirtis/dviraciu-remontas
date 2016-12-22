a19a1a357c0b2c43efc814bc0ca5254708c2ca4e
Modestas <mode.saulys@gmail.com>
2016-12-22 16:27:38 +0200

    wage saving fix

--- server/wage/save.sql
+++ server/wage/save.sql
@@ -18,20 +18,13 @@ insert into `wage` (
     '{date}',
     {accountant_id},
     {id}
-) on duplicate key update
-    sum = values(sum),
-    work_hours = values(work_hours),
-    deductibles = values(deductibles),
-    pvm = values(pvm),
-    comment = values(comment),
-    penalty = values(penalty),
-    date_confirmed = values(date_confirmed),
-    accountant_id = values(accountant_id),
-    mechanic_id = values(mechanic_id);
+);
+
+set @wageid = LAST_INSERT_ID();
 
 update `mechanic`
 set date_last_payed = NOW()
 where id = {id};
 
 insert into `advance_payment` (sum, date, comment, wage_id)
-values ({sum}, NOW(), '{comment}', {id});
+values ({sum}, NOW(), '{comment}', @wageid);

2cc97fa3aba6cd845d7e424bc9c44182d680bdcf
Gvidas Janušonis <gvidasja@gmail.com>
2016-12-22 03:16:37 +0200

    fix report

--- app/wage-report/wage-report-edit-controller.js
+++ app/wage-report/wage-report-edit-controller.js
@@ -2,11 +2,21 @@
 export function WageReportController($controller, WagesReportService) {
     let self = this;
     $controller('BaseController', { vm: self });
+
+    let now = new Date();
+
+    self.model = {
+        from: now,
+        to: new Date(now.setMonth(now.getMonth() - 1))
+    };
+
     this.submit = function submit() {
-        var tempFrom = new Date(self.model.from);
-        var tempTo = new Date(self.model.to);
+        let tempFrom = new Date( self.model.from );
+        let tempTo = new Date( self.model.to );
+
         tempFrom = tempFrom.toISOString().slice(0,10).replace(/-/g,"-");
         tempTo = tempTo.toISOString().slice(0,10).replace(/-/g,"-");
+
         self.model.from = tempFrom;
         self.model.to = tempTo;
         self.clearError();

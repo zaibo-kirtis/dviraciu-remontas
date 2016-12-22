bb9e29fc70216494105218200ac079de78a6f429
Gvidas Janušonis <gvidasja@gmail.com>
2016-12-22 02:50:58 +0200

    remove cities

--- app/menu-items.js
+++ app/menu-items.js
@@ -1,7 +1,6 @@
 export let MenuItems = [
     { route: 'orders', display: 'Užsakymai', permissions: ['client', 'mechanic'] },
     { route: 'bikes', display: 'Dviračiai', permissions: ['client'] },
-    { route: 'cities', display: 'Miestai', permissions: ['admin'] },
     { route: 'mechanics', display: 'Meistrai', permissions: ['admin'] },
     { route: 'parts', display: 'Dalys', permissions: ['admin'] },
     { route: 'payments', display: 'Mokėjimai', permissions: ['accountant', 'client'] },

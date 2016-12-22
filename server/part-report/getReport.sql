48c2fcbb628fb66a47db6d7d3816d16e366d1c56
Ineta Jurkutė <jurkute.ineta@gmail.com>
2016-12-22 16:28:41 +0200

    part report fix

--- server/part-report/getReport.sql
+++ server/part-report/getReport.sql
@@ -1,4 +1,6 @@
 SELECT
+`order`.id as order_id,
+date_created,
 part.price,
 part.name,
 part.manufacturer,
@@ -8,3 +10,4 @@ FROM `order`
 join part_order as po on po.order_id = id
 join part as part on po.part_id = part.id
 WHERE date_created > '{from}' AND date_created < '{to}'
+order by order_id

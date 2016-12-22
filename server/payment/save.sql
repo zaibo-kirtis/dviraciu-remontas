d8da4afe2dff0ba107a872077b71a2d4ae7d6664
Gvidas Janušonis <gvidasja@gmail.com>
2016-12-22 14:59:35 +0200

    add receipt state change on payments

--- server/payment/save.sql
+++ server/payment/save.sql
@@ -1,15 +1,25 @@
 insert into `payment` (
+    id,
     sum,
     date,
     comment,
     receipt_id
 ) values (
+    {id},
     {sum},
     NOW(),
     '{comment}',
     {receipt_id}
 ) on duplicate key update
+    id = last_insert_id(id),
     sum = values(sum),
     date = NOW(),
     comment = values(comment),
     receipt_id = values(receipt_id);
+
+set @sum = (select sum from receipt where id = {receipt_id}) - (select sum(sum) from payment where receipt_id = {receipt_id});
+
+set @state = (select if(@sum > 0, 1, 2));
+
+update receipt set receipt_state_id = @state where id = {receipt_id};
+

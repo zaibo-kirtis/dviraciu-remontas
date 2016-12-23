cd88a89fcbdd9e591739cdc5da75fc3d15aeaf85
Gvidas Janušonis <gvidasja@gmail.com>
2016-12-23 12:11:13 +0200

    move injection detection to string replacing

--- server/helpers.js
+++ server/helpers.js
@@ -2,14 +2,10 @@ let helpers = {
     insertData: ( query, object ) => {
         for( let property in object ) {
             object[ property ] = object[ property ] || null;
-
-            if(typeof(object[property]) === 'string') {
-                object[property] = preventInjection(object[property]);
-            }
         }
 
         query = query
-            .replace( /'{(\w*)}'/g, ( match, key ) => object[ key ] ? `'${object[ key ]}'` : null )
+            .replace( /'{(\w*)}'/g, ( match, key ) => object[ key ] ? `'${preventInjection(object[ key ])}'` : null )
             .replace( /{(\w*)}/g, ( match, key ) => object[ key ] || null );
 
         console.log(query);
@@ -27,7 +23,11 @@ let helpers = {
 };
 
 function preventInjection(str) {
-    return str.replace(/['"\b\n\r\t\\%_]/g, '');
+    if(typeof(str) !== 'string') {
+        return str;
+    } else {
+        return str.replace(/['"\b\n\r\t\\%_]/g, '');
+    }
 }
 
 module.exports = helpers;

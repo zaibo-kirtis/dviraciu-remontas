30c848319a4cbea62ffeb49592929901544a1f12
Ubuntu <ubuntu@ip-172-31-17-30.eu-central-1.compute.internal>
2017-08-16 07:55:02 +0000

    add build scripts

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ service_name="dviraciu-remontas"
 sudo service $service_name stop
 git pull
 npm install
+npm run-script build
 sudo \cp -f $service_name.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable $service_name
